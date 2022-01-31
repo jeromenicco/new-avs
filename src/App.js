aab30f012c45061b97094e772fbd10debeda5d05
Jerome Nicco <jeromenicco@MacBook-Air-de-Jerome.local>
2022-01-31 14:10:19 +0100

    responsive news section

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   const isMobile = useMediaQuery({ query: '(max-width: 600px)' })
   const isTablet = useMediaQuery({ query: '(min-width: 600px)' && '(max-width: 900px)' })
-  const isLaptop = useMediaQuery({ query: '(min-width: 1000px)' })
+  const isLaptop = useMediaQuery({ query: '(min-width: 900px)' })
   const isBigScreen = useMediaQuery({ query: '(min-width: 1900px)' })
 
   console.log(isMobile)
